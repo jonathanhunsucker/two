5a5806b2b0c3f6e1f3713e5feb4172dd51a28f0c
Jonathan Hunsucker <jonathantrevorh@gmail.com>
2019-10-19 15:32:17 -0400

    initial commit

---
+++ test/index.js
@@ -0,0 +1,11 @@
+"use strict";
+
+var assert = require('assert');
+
+var two = require('../src/index');
+
+describe('two', function () {
+  it('should be two', function () {
+    assert.equal(two, 2);
+  });
+});
